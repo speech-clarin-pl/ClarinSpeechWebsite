af9a5b5b79e8b1727bb1eb60e44080a0a103e14e
Danijel Koržinek <danijel@pja.edu.pl>
2017-12-08 22:41:06 +0100

    Load config from file.

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
+import json
 import os
 
 from pymongo import MongoClient
 
 
-# TODO -- argparse
 class Config:
     def __init__(self):
         self.proj_root = os.path.dirname(os.path.realpath(__file__))
@@ -15,6 +15,29 @@ class Config:
         self.emu.master_password = 'masterpassword'
         self.corpora_dir = '/dane/korpusy'
 
+    def load(self, config_path):
+        try:
+            config_file = json.load(config_path)
+        except:
+            return
+
+        if 'proj_root' in config_file:
+            self.proj_root = config_file['proj_root']
+        if 'work_dir' in config_file:
+            self.work_dir = config_file['work_dir']
+        if 'phonetisaurus_model' in config_file:
+            self.phonetisaurus_model = config_file['phonetisaurus_model']
+        if 'emu' in config_file:
+            if 'proj_root' in config_file['emu']:
+                self.emu.projects_per_page = config_file['emu']['projects_per_page']
+            if 'webapp_port' in config_file['emu']:
+                self.emu.webapp_port = config_file['emu']['webapp_port']
+            if 'master_password' in config_file['emu']:
+                self.emu.master_password = config_file['emu']['master_password']
+        if 'corpora_dir' in config_file:
+            self.corpora_dir = config_file['corpora_dir']
+
 
 config = Config()
+config.load(os.path.join(config.proj_root, 'config.json'))
 db = MongoClient()
